4c82aee90b9c1a3368fd113664667e33685d9432
pietikainen <pietikainen@gmail.com>
2024-01-26 22:05:14 +0200

    MySQL connection tester working \o/

--- varausjarjestelma/Database/MySqlHelper.cs
+++ varausjarjestelma/Database/MySqlHelper.cs
@@ -7,40 +7,77 @@ using System.Text;
 using System.Threading.Tasks;
 using MySql.Data;
 using MySql.Data.MySqlClient;
+using MySql.Data.MySqlClient.Authentication;
+using System.Data.Common;
 
 
 namespace varausjarjestelma.Database
 {
     public class MySqlHelper
     {
-        private readonly string connString = "server=ohjelmisto1-sql-pietikainen-6a40.a.aivencloud.com; port=12244; database=vn; user=kayttaja; password=AVNS_DpLuKO1gwxqxgTFe1dy";
-        
-        public MySqlHelper( string connString )
+        private readonly MySqlConnectionStringBuilder connectionStringBuilder = new MySqlConnectionStringBuilder
         {
-            this.connString = connString;
-        }
+            Server = "ohjelmisto1-sql-pietikainen-6a40.a.aivencloud.com",
+            UserID = "kayttaja",
+            Password = "AVNS_DpLuKO1gwxqxgTFe1dy",
+            Database = "vn",
+            Port = 11244
+        };
+
         public MySqlHelper()
-        {
+        {        
         }
 
-        public bool TestConnection()
+        public async Task<bool> TestConnectionAsync()
         {
             MySql.Data.MySqlClient.MySqlConnection conn;
-            {
-                try
-                {
-                    conn = new MySql.Data.MySqlClient.MySqlConnection(connString);
-                    conn.Open();
-                    return true;
-                }
-                catch (MySqlException ex)
-                {
-                    Debug.WriteLine(ex.Message);
-                    return false;
-                }
 
+            try
+            {
+                conn = new MySql.Data.MySqlClient.MySqlConnection();
+                conn.ConnectionString = connectionStringBuilder.ConnectionString;
+                await conn.OpenAsync();
+                return true;
+            }
+            catch (MySql.Data.MySqlClient.MySqlException ex)
+            {
+                Debug.WriteLine(ex.Message);
+                return false;
+            }
+
+        }
+
+        public async Task<List<AlueData>> GetAllAlueDataAsync()
+        {
+            using (var connection = new MySqlConnection(connectionStringBuilder.ConnectionString))
+            {
+                await connection.OpenAsync();
+
+                using (var command = new MySqlCommand("SELECT * FROM alue", connection))
+                using (var reader = await command.ExecuteReaderAsync())
+                {
+                    List<AlueData> alueDataList = new List<AlueData>();
+
+                    while (await reader.ReadAsync())
+                    {
+                        AlueData alueData = new AlueData
+                        {
+                            AlueId = reader.GetInt32("alue_id"),
+                            Nimi = reader.GetString("nimi")
+                        };
+
+                        alueDataList.Add(alueData);
+                    }
+
+                    return alueDataList;
+                }
             }
         }
 
+        public class AlueData
+        {
+            public int AlueId { get; set; }
+            public string Nimi { get; set; }
+        }
     }
 }
--- varausjarjestelma/MainPage.xaml.cs
+++ varausjarjestelma/MainPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using Mysqlx;
 using Org.BouncyCastle.Tls;
+using System.Diagnostics;
 
 namespace varausjarjestelma
 {
@@ -24,13 +25,52 @@ namespace varausjarjestelma
             SemanticScreenReader.Announce(CounterBtn.Text);
         }
 
-        private void OnDatabaseTestButtonClicked(object sender, EventArgs e)
+        private async void OnDatabaseTestButtonClicked(object sender, EventArgs e)
         {
-            var helper = new Database.MySqlHelper();
-            var isConnected = helper.TestConnection();
+            try
+            {
+                var helper = new Database.MySqlHelper();
+                var isConnected = await helper.TestConnectionAsync();
 
-            MySqlTestLabel.Text = isConnected ? "Connected successfully" : "Connection failed"; 
+                MySqlTestLabel.Text = isConnected ? "Connected successfully" : "Connection failed";
+            }
+            catch (AggregateException ae)
+            {
+                foreach (var innerException in ae.InnerExceptions)
+                {
+                    Debug.WriteLine($"Inner Exception: {innerException.Message}");
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine($"An error occurred: {ex.Message}");
+            }
         }
+
+        private async void OnDatabaseReadButtonClicked(object sender, EventArgs e)
+        {
+            try
+            {
+                var helper = new Database.MySqlHelper();
+                var alueDataList = await helper.GetAllAlueDataAsync();
+
+                MySqlTestLabel.Text = $"Found {alueDataList.Count} rows";
+            }
+            catch (AggregateException ae)
+            {
+                foreach (var innerException in ae.InnerExceptions)
+                {
+                    Debug.WriteLine($"Inner Exception: {innerException.Message}");
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine($"An error occurred: {ex.Message}");
+
+            }
+        }
+
     }
 
 }
