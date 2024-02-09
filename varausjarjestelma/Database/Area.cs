7752d9614f04986c8e5fc82b5f770f57f93723ec
pietikainen <pietikainen@gmail.com>
2024-02-09 12:46:42 +0200

    created classes for mysql tables

---
+++ varausjarjestelma/Database/Area.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace varausjarjestelma.Database
+{
+    public class Area
+    {
+        [Key]
+        public int alue_id { get; set; }
+        public string nimi { get; set; }
+    }
+}
---
+++ varausjarjestelma/Database/ServiceOnReservation.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace varausjarjestelma.Database
+{
+    public class ServiceOnReservation
+    {
+        [Key]
+        public int varaus_id { get; set; }
+        public int palvelu_id { get; set; }
+        public int lkm { get; set; }
+    }
+}
