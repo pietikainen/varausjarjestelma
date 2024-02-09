7a9d265126d8657b571cd1547ed2cebb239913ac
pietikainen <pietikainen@gmail.com>
2024-02-09 12:51:33 +0200

    created AppContext.cs for Entity Framework Core

---
+++ varausjarjestelma/Database/AppContext.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+//using MySql.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace varausjarjestelma.Database
+{
+    public class AppContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySQL("Server=ohjelmisto1-sql-pietikainen-6a40.a.aivencloud.com;Database=vn;User=kayttaja;Password=AVNS_DpLuKO1gwxqxgTFe1dy;Port=11244;");
+        }
+
+        public AppContext()
+        {
+        }
+
+        public DbSet<Area> alue { get; set; }
+        public DbSet<Customer> asiakas { get; set; }
+        public DbSet<Invoice> lasku { get; set; }
+        public DbSet<Cabin> mokki { get; set; }
+        public DbSet<Service> palvelu { get; set; }
+        public DbSet<PostalCode> posti { get; set; }
+        public DbSet<ServiceOnReservation> varauksen_palvelut { get; set; }
+        public DbSet<Reservation> varaus { get; set; }
+
+    }
+}
