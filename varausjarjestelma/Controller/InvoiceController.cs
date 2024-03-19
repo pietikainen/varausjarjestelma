ad0cfcff0b25b88f38e5866d36a62855967cc323
pietikainen <pietikainen@gmail.com>
2024-03-19 16:21:48 +0200

    Invoice printing to screen now implemented. Really nicely done with grids etc. Populates Invoice element now when clicked from Listview.
    Introduced AlterInvoicePaidStatus() method.

--- varausjarjestelma/Controller/InvoiceController.cs
+++ varausjarjestelma/Controller/InvoiceController.cs
@@ -9,7 +9,7 @@ using System.Diagnostics;
 using System.Threading.Tasks;
 using MySql.Data.MySqlClient;
 using Microsoft.EntityFrameworkCore;
-
+using varausjarjestelma.Database;
 
 namespace varausjarjestelma.Controller
 {
@@ -44,17 +44,10 @@ namespace varausjarjestelma.Controller
             await connection.OpenAsync();
 
             using (var command = new MySqlCommand(
-                @"SELECT
-                        l.lasku_id,
-                        concat(a.sukunimi, ' ', a.etunimi) AS asiakas,
-                     l.summa,
-                     l.maksettu
-                    FROM 
-                     lasku l
-                    JOIN
-                    	varaus v ON l.varaus_id = v.varaus_id
-                    JOIN
-                    	asiakas a ON v.asiakas_id = a.asiakas_id;", connection))
+                @"SELECT l.lasku_id, a.sukunimi, a.etunimi, l.summa, l.maksettu
+                    FROM lasku l
+                    JOIN varaus v ON l.varaus_id = v.varaus_id
+                    JOIN asiakas a ON v.asiakas_id = a.asiakas_id;", connection))
 
             using (var reader = await command.ExecuteReaderAsync())
             {
@@ -65,7 +58,9 @@ namespace varausjarjestelma.Controller
                     InvoiceData invoiceData = new InvoiceData
                     {
                         InvoiceNumber = reader.GetInt32("lasku_id"),
-                        CustomerName = reader.GetString("asiakas"),
+                        FirstName = reader.GetString("etunimi"),
+                        LastName = reader.GetString("sukunimi"),
+                        CustomerName = reader.GetString("sukunimi") + " " + reader.GetString("etunimi"),
                         InvoiceAmount = reader.GetDouble("summa"),
                         IsPaid = reader.GetInt32("maksettu")
                     };
@@ -76,6 +71,156 @@ namespace varausjarjestelma.Controller
 
             }
         }
+
+        public static async Task<InvoiceDetails> GetFullInvoiceAsync(int id)
+        {
+            MySqlConnection connection = MySqlController.GetConnection();
+            await connection.OpenAsync();
+
+            using (var command = new MySqlCommand(
+                @"SELECT a.etunimi, a.sukunimi, a.lahiosoite, a.postinro, p.toimipaikka, a.email, a.puhelinnro,
+                v.varaus_id, v.asiakas_id, v.mokki_mokki_id, v.varattu_alkupvm, v.varattu_loppupvm,
+                m.mokki_id, m.mokkinimi, m.hinta as mokkihinta,
+                l.lasku_id, l.summa, l.alv as laskualv
+                FROM lasku l
+                INNER JOIN varaus v ON l.varaus_id = v.varaus_id
+                LEFT JOIN asiakas a ON v.asiakas_id = a.asiakas_id
+                LEFT JOIN mokki m ON v.mokki_mokki_id = m.mokki_id
+                LEFT JOIN posti p ON a.postinro = p.postinro
+                WHERE l.lasku_id = @id;", connection))
+            {
+                command.Parameters.AddWithValue("@id", id);
+
+                using (var reader = await command.ExecuteReaderAsync())
+                {
+                    if (await reader.ReadAsync())
+                    {
+                        InvoiceDetails fullInvoice = new InvoiceDetails
+                        {
+                            CustomerId = reader.GetInt32("asiakas_id"),
+                            FullName = reader.GetString("etunimi") + " " + reader.GetString("sukunimi"),
+                            Address = reader.GetString("lahiosoite"),
+                            PostalCode = reader.GetString("postinro"),
+                            City = reader.GetString("toimipaikka"),
+                            PostalCodeAndCity = reader.GetString("postinro") + " " + reader.GetString("toimipaikka"),
+                            Email = reader.GetString("email"),
+                            Phone = reader.GetString("puhelinnro"),
+
+                            CabinId = reader.GetInt32("mokki_id"),
+                            CabinName = reader.GetString("mokkinimi"),
+                            StartDate = reader.GetDateTime("varattu_alkupvm"),
+                            EndDate = reader.GetDateTime("varattu_loppupvm"),
+                            Days = (reader.GetDateTime("varattu_loppupvm") - reader.GetDateTime("varattu_alkupvm")).Days,
+                            CabinPrice = reader.GetDouble("mokkihinta"),
+                            TotalCabinPrice = reader.GetDouble("mokkihinta") * (reader.GetDateTime("varattu_loppupvm") - reader.GetDateTime("varattu_alkupvm")).Days,
+
+                            InvoiceId = reader.GetInt32("lasku_id"),
+                            InvoiceDate = DateTime.Today.ToString("d"),
+                            DueDate = DateTime.Today.AddDays(14).ToString("d"),
+                            Total = reader.GetDouble("summa"),
+                            Tax = reader.GetInt32("laskualv"),
+
+                        };
+
+                        await connection.CloseAsync();
+                        return fullInvoice;
+                    }
+                }
+            }
+
+            await connection.CloseAsync();
+            return null; // Palauta null, jos laskua ei löydy
+        }
+
+        public static async Task<List<ServicesOnReservation>> GetServicesOnReservationAsync(int id)
+        {
+            MySqlConnection connection = MySqlController.GetConnection();
+            await connection.OpenAsync();
+            Debug.WriteLine("Trying to get services on reservation from database");
+
+            using (var command = new MySqlCommand(
+                @"SELECT p.nimi, p.hinta, vp.lkm, p.alv
+                    FROM palvelu p
+                    JOIN varauksen_palvelut vp ON vp.palvelu_id = p.palvelu_id
+                    JOIN lasku l ON vp.varaus_id = l.varaus_id
+                    WHERE l.lasku_id = @id;", connection))
+            {
+                command.Parameters.AddWithValue("@id", id);
+
+                using (var reader = await command.ExecuteReaderAsync())
+                {
+                    List<ServicesOnReservation> serviceListOnReservation = new List<ServicesOnReservation>();
+
+                    while (await reader.ReadAsync())
+                    {
+                        ServicesOnReservation servicesOnReservation = new ServicesOnReservation
+                        {
+                            ServiceName = reader.GetString("nimi"),
+                            ServiceAmount = reader.GetInt32("lkm"),
+                            ServicePrice = reader.GetDouble("hinta"),
+                            ServiceVat = reader.GetInt32("alv")
+                        };
+                        serviceListOnReservation.Add(servicesOnReservation);
+                    }
+                    await connection.CloseAsync();
+                    if (serviceListOnReservation.Count == 0)
+                    {
+                        Debug.WriteLine("No services on reservation found from database");
+                    }
+                    else
+                    {
+                        Debug.WriteLine("Got services on reservation from database");
+                    }
+                    return serviceListOnReservation;
+                }
+            }
+        }
+
+        public static async Task<List<InvoiceContentsCabin>> GetCabinsOnReservationAsync(int id)
+        {
+            MySqlConnection connection = MySqlController.GetConnection();
+            await connection.OpenAsync();
+            Debug.WriteLine("Trying to get cabins on reservation from database");
+
+            using (var command = new MySqlCommand(
+                @"SELECT m.mokki_id, m.mokkinimi, m.hinta, v.varattu_alkupvm, v.varattu_loppupvm, l.alv
+                    FROM mokki m
+                    JOIN varaus v ON m.mokki_id = v.mokki_mokki_id
+                    JOIN lasku l ON v.varaus_id = l.varaus_id
+                    WHERE l.lasku_id = @id;", connection))
+            {
+                command.Parameters.AddWithValue("@id", id);
+
+                using (var reader = await command.ExecuteReaderAsync())
+                {
+                    List<InvoiceContentsCabin> listOfCabins = new List<InvoiceContentsCabin>();
+
+                    if (await reader.ReadAsync())
+                    {
+                        InvoiceContentsCabin contents = new InvoiceContentsCabin
+                        {
+                            CabinId = reader.GetInt32("mokki_id"),
+                            CabinName = reader.GetString("mokkinimi"),
+                            CabinAmount = (reader.GetDateTime("varattu_loppupvm") - reader.GetDateTime("varattu_alkupvm")).Days,
+                            CabinPrice = reader.GetDouble("hinta"),
+                            CabinVat = reader.GetInt32("alv")
+                        };
+                        listOfCabins.Add(contents);
+                    }
+                    await connection.CloseAsync();
+                    if (listOfCabins.Count == 0)
+                    {
+                        Debug.WriteLine("No cabins on reservation found from database");
+                    }
+                    else
+                    {
+                        Debug.WriteLine("Got cabins on reservation from database");
+                    }
+                    return listOfCabins;
+                }
+            }
+        }
+
 
 
         public async Task<List<Database.Invoice>> GetInvoiceByNumber(int id)
@@ -116,46 +261,155 @@ namespace varausjarjestelma.Controller
             }
         }
 
+        public async Task<List<InvoiceData>> FilterInvoicesPreviewData(string filter)
+        {
+            MySqlConnection connection = MySqlController.GetConnection();
+            await connection.OpenAsync();
+
+            using (var command = new MySqlCommand(
+                               @"SELECT
+                        l.lasku_id,
+                        a.sukunimi, a.etunimi,
+                     l.summa,
+                     l.maksettu
+                    FROM 
+                     lasku l
+                    JOIN
+                    	varaus v ON l.varaus_id = v.varaus_id
+                    JOIN
+                    	asiakas a ON v.asiakas_id = a.asiakas_id
+                    WHERE
+                    	a.sukunimi LIKE @filter OR a.etunimi LIKE @filter; OR l.lasku_id LIKE @filter", connection))
+            {
+                command.Parameters.AddWithValue("@filter", "%" + filter + "%");
+
+                using (var reader = await command.ExecuteReaderAsync())
+                {
+                    List<InvoiceData> invoiceDataList = new List<InvoiceData>();
+
+                    while (await reader.ReadAsync())
+                    {
+                        InvoiceData invoiceData = new InvoiceData
+                        {
+                            InvoiceNumber = reader.GetInt32("lasku_id"),
+                            FirstName = reader.GetString("a.etunimi"),
+                            LastName = reader.GetString("a.sukunimi"),
+                            CustomerName = reader.GetString("a.sukunimi") + " " + reader.GetString("a.etunimi"),
+                            InvoiceAmount = reader.GetDouble("summa"),
+                            IsPaid = reader.GetInt32("maksettu")
+                        };
+                        invoiceDataList.Add(invoiceData);
+                    }
+                    await connection.CloseAsync();
+                    return invoiceDataList;
+                }
+            }
+        }
+
+        public async Task<bool> AlterInvoicePaidStatus(int id)
+        {
+            using (var context = new Database.AppContext())
+            {
+                try
+                {
+                    Debug.WriteLine("Trying to get invoices from database");
+
+                    if (context.Database.CanConnect())
+                    {
+                        var invoice = await context.lasku.Where(i => i.lasku_id == id).FirstOrDefaultAsync();
+                        Debug.WriteLine("Got invoice from database");
+                        Debug.WriteLine("invoice id: " + invoice.ToString());
+
+                        if (invoice != null)
+                        {
+                            invoice.maksettu = 1;
+                            await context.SaveChangesAsync();
+                            return true;
+                        }
+                        else
+                        {
+                            return false;
+                        }
+                    }
+                    else
+                    {
+                        Debug.WriteLine("Database connection is not open.");
+                        return false;
+                    }
+                }
+                catch (Exception e)
+                {
+                    Debug.WriteLine("Error getting invoices from database:");
+                    Debug.WriteLine(e.Message);
+                    return false;
+                }
+            }
+        }
     }
 
 
     public class InvoiceData
     {
         public int InvoiceNumber { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
         public string CustomerName { get; set; }
         public double InvoiceAmount { get; set; }
-        public double Vat { get; set; }
+        public int Vat { get; set; }
         public int IsPaid { get; set; }
     }
 
+    public class InvoiceContentsCabin
+    {
+        public int CabinId { get; set; }
+        public string CabinName { get; set; }
+        public int CabinAmount { get; set; }
+        public double CabinPrice { get; set; }
+        public double CabinVat { get; set; }
 
+    }
 
     public class InvoiceDetails
-        {
-            // Customer
-            public int CustomerId { get; set; }
-            public required string FullName { get; set; }
-            public required string Address { get; set; }
-            public required string PostalCode { get; set; }
-            public required string Email { get; set; }
-            public required string Phone { get; set; }
+    {
 
-            // Cabin
-            public int CabinId { get; set; }
-            public required string CabinName { get; set; }
-            public DateTime StartDate { get; set; }
-            public DateTime EndDate { get; set; }
-            public int Price { get; set; }
+        // Customer
+        public int CustomerId { get; set; }
+        public required string FullName { get; set; }
+        public required string Address { get; set; }
+        public required string PostalCode { get; set; }
+        public required string City { get; set; }
+        public required string PostalCodeAndCity { get; set; }
+        public required string Email { get; set; }
+        public required string Phone { get; set; }
 
-            // Invoice
-            public int InvoiceId { get; set; }
-            public required string InvoiceDate { get; set; }
-            public required string DueDate { get; set; }
-            public int Total { get; set; }
-            public int Tax { get; set; }
+        // Cabin
+        public int CabinId { get; set; }
+        public required string CabinName { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public int Days { get; set; }
+        public double CabinPrice { get; set; }
+        public double TotalCabinPrice { get; set; }
 
-            // Services
+        // Invoice
+        public int InvoiceId { get; set; }
+        public required string InvoiceDate { get; set; }
+        public required string DueDate { get; set; }
+        public double Total { get; set; }
+        public int Tax { get; set; }
 
-        }
+        // Services helper
+
+    }
+
+    public class ServicesOnReservation
+    {
+        public int ServiceAmount { get; set; }
+        public string ServiceName { get; set; }
+        public double ServicePrice { get; set; }
+        public double ServiceVat { get; set; }
+
+
+    }
 
 }
--- varausjarjestelma/Invoice.xaml.cs
+++ varausjarjestelma/Invoice.xaml.cs
@@ -1,20 +1,17 @@
 using Microsoft.Maui.Controls.Platform;
 using System.Diagnostics;
+using System.Drawing;
 using varausjarjestelma.Controller;
 
 namespace varausjarjestelma;
 
 public partial class Invoice : ContentPage
 {
+    public List<InvoiceData> invoices;
+
     public Invoice()
-	{
-        // Asetetaan sivun BindingContext InvoiceViewModel-olioon.
-        // T‰m‰ mahdollistaa XAML-tiedostossa m‰‰riteltyjen elementtien datan sitomisen
-        // InvoiceViewModelin ominaisuuksiin k‰ytt‰m‰ll‰ {Binding}-syntaksia.
-
+    {
         InitializeComponent();
-        BindingContext = new InvoiceViewModel();
-
     }
 
     protected override void OnAppearing()
@@ -28,10 +25,9 @@ public partial class Invoice : ContentPage
     {
         try
         {
-            var helper = new InvoiceController();
-            var invoices = await helper.GetAllInvoicesPreviewAsync();
+            var controller = new InvoiceController();
+            invoices = await controller.GetAllInvoicesPreviewAsync();
             InvoicesListView.ItemsSource = invoices;
-
         }
         catch (AggregateException ae)
         {
@@ -56,16 +52,115 @@ public partial class Invoice : ContentPage
                 Debug.WriteLine("e.Item: " + e.Item);
                 return;
             }
-                
 
-            Debug.WriteLine("Selected invoice: " + selectedInvoice);
+            // Populate invoice details page with selected invoice data
+            BindingContext = await InvoiceController.GetFullInvoiceAsync(selectedInvoice.InvoiceNumber);
 
-            var invoiceId = selectedInvoice.InvoiceNumber;
-            var popupPage = new InvoiceDetails(invoiceId);
+            // Bind the grid
+            var InvoiceGrid_ContentPart = this.FindByName<Grid>("InvoiceGrid_ContentPart");
 
-            Console.WriteLine("Selected invoice: " + selectedInvoice);
+            // Clear the InvoiceGrid_ContentPart
+            InvoiceGrid_ContentPart.Children.Clear();
+            InvoiceGrid_ContentPart.RowDefinitions.Clear();
+            InvoiceGrid_ContentPart.ColumnDefinitions.Clear();
 
-            await Navigation.PushAsync(popupPage);
+            // Generate new column definitions x4
+
+            for (int i = 0; i < 5; i++)
+            {
+                var columnDefinition = new ColumnDefinition();
+                columnDefinition.Width = new GridLength(160);
+                InvoiceGrid_ContentPart.ColumnDefinitions.Add(columnDefinition);
+            }
+
+            // Populate Invoice contents with selected invoice data
+            var cabins = await InvoiceController.GetCabinsOnReservationAsync(selectedInvoice.InvoiceNumber);
+
+            foreach (var cabin in cabins)
+            {
+                int row = 0;
+
+                var cabinNameLabel = new Label { Text = "Mˆkki: " + cabin.CabinName };
+                var cabinAmountLabel = new Label { Text = cabin.CabinAmount.ToString() + " vrk" };
+                var cabinPriceLabel = new Label { Text = cabin.CabinPrice.ToString("C") };
+                var cabinVatLabel = new Label { Text = cabin.CabinVat.ToString() };
+                var cabinTotal = (double)cabin.CabinAmount * (double)cabin.CabinPrice;
+                var cabinTotalLabel = new Label { Text = cabinTotal.ToString("C") };
+
+                // Generate new row to the grid
+                var rowDefinition = new RowDefinition();
+                rowDefinition.Height = GridLength.Auto;
+                InvoiceGrid_ContentPart.RowDefinitions.Add(rowDefinition);
+
+                // Add labels to grid and set their location
+                InvoiceGrid_ContentPart.Children.Add(cabinNameLabel);
+                Grid.SetRow(cabinNameLabel, InvoiceGrid_ContentPart.RowDefinitions.Count - 1);
+                Grid.SetColumn(cabinNameLabel, 0);
+
+                InvoiceGrid_ContentPart.Children.Add(cabinAmountLabel);
+                Grid.SetRow(cabinAmountLabel, InvoiceGrid_ContentPart.RowDefinitions.Count - 1);
+                Grid.SetColumn(cabinAmountLabel, 1);
+
+                InvoiceGrid_ContentPart.Children.Add(cabinPriceLabel);
+                Grid.SetRow(cabinPriceLabel, InvoiceGrid_ContentPart.RowDefinitions.Count - 1);
+                Grid.SetColumn(cabinPriceLabel, 2);
+
+                InvoiceGrid_ContentPart.Children.Add(cabinVatLabel);
+                Grid.SetRow(cabinVatLabel, InvoiceGrid_ContentPart.RowDefinitions.Count - 1);
+                Grid.SetColumn(cabinVatLabel, 3);
+
+                InvoiceGrid_ContentPart.Children.Add(cabinTotalLabel);
+                Grid.SetRow(cabinTotalLabel, InvoiceGrid_ContentPart.RowDefinitions.Count - 1);
+                Grid.SetColumn(cabinTotalLabel, 4);
+
+                // Generate empty row with no columns and height of 20px
+                var emptyRowDefinition = new RowDefinition();
+                emptyRowDefinition.Height = new GridLength(20);
+                InvoiceGrid_ContentPart.RowDefinitions.Add(emptyRowDefinition);
+            }
+
+
+            var services = await InvoiceController.GetServicesOnReservationAsync(selectedInvoice.InvoiceNumber);
+
+            foreach (var service in services)
+            {
+                int row = InvoiceGrid_ContentPart.RowDefinitions.Count - 1; // Seuraava rivi
+
+                var serviceNameLabel = new Label { Text = service.ServiceName };
+                var serviceAmountLabel = new Label { Text = service.ServiceAmount.ToString() + " kpl" };
+                var servicePriceLabel = new Label { Text = service.ServicePrice.ToString() };
+                var serviceVatLabel = new Label { Text = service.ServiceVat.ToString() };
+                var serviceTotal = (double)service.ServiceAmount * (double)service.ServicePrice;
+                var serviceTotalLabel = new Label { Text = serviceTotal.ToString() };
+
+                // Generate new row to the grid
+                var rowDefinition = new RowDefinition();
+                rowDefinition.Height = GridLength.Auto;
+                InvoiceGrid_ContentPart.RowDefinitions.Add(rowDefinition);
+
+                // Add labels to grid and set their location
+
+                InvoiceGrid_ContentPart.Children.Add(serviceNameLabel);
+                Grid.SetRow(serviceNameLabel, InvoiceGrid_ContentPart.RowDefinitions.Count - 1);
+                Grid.SetColumn(serviceNameLabel, 0);
+
+                InvoiceGrid_ContentPart.Children.Add(serviceAmountLabel);
+                Grid.SetRow(serviceAmountLabel, InvoiceGrid_ContentPart.RowDefinitions.Count - 1);
+                Grid.SetColumn(serviceAmountLabel, 1);
+
+                InvoiceGrid_ContentPart.Children.Add(servicePriceLabel);
+                Grid.SetRow(servicePriceLabel, InvoiceGrid_ContentPart.RowDefinitions.Count - 1);
+                Grid.SetColumn(servicePriceLabel, 2);
+
+                InvoiceGrid_ContentPart.Children.Add(serviceVatLabel);
+                Grid.SetRow(serviceVatLabel, InvoiceGrid_ContentPart.RowDefinitions.Count - 1);
+                Grid.SetColumn(serviceVatLabel, 3);
+
+                InvoiceGrid_ContentPart.Children.Add(serviceTotalLabel);
+                Grid.SetRow(serviceTotalLabel, InvoiceGrid_ContentPart.RowDefinitions.Count - 1);
+                Grid.SetColumn(serviceTotalLabel, 4);
+            }
+
         }
         catch (Exception ex)
         {
@@ -75,20 +170,23 @@ public partial class Invoice : ContentPage
 
 
 
+    // Code to filter invoices by customer name and/or invoice number using List<InvoiceData> invoices
     private void FilterInvoicesByNumber(object sender, EventArgs e)
     {
-
-
+        var filterParameter = InvoiceNumberEntry.Text;
+        var filteredInvoices = invoices.Where(i => i.InvoiceNumber.ToString().Contains(filterParameter)).ToList();
+        InvoicesListView.ItemsSource = filteredInvoices;
     }
 
     private void FilterInvoicesByName(object sender, EventArgs e)
     {
-
+        var filterParameter = CustomerNameEntry.Text;
+        var filteredInvoices = invoices.Where(i => i.CustomerName.Contains(filterParameter)).ToList();
+        InvoicesListView.ItemsSource = filteredInvoices;
     }
 
 
     // Button clicked event handlers start here
-
     public void OnSearchButtonClicked(object sender, EventArgs e)
     {
         if (InvoiceNumberEntry.Text != null)
@@ -110,7 +208,7 @@ public partial class Invoice : ContentPage
 
     async void MainMenuButtonClicked(object sender, EventArgs e)
     {
-       await Navigation.PushAsync(new MainPage());
+        await Navigation.PushAsync(new MainPage());
     }
 
     private void PrintInvoiceButtonClicked(object sender, EventArgs e)
