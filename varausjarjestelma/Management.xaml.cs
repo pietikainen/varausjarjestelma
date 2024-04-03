567fc874d7390ba061dc2f8a71a2f71b8b8b524b
Kniippala <niko.jarvela@hotmail.fi>
2024-04-03 17:01:05 +0300

    New management page build

--- varausjarjestelma/Management.xaml.cs
+++ varausjarjestelma/Management.xaml.cs
@@ -7,17 +7,40 @@ public partial class Management : ContentPage
 		InitializeComponent();
 	}
 
-    private void AddButtonClicked(object sender, EventArgs e)
+    private void Picker_SelectedIndexChanged(object sender, EventArgs e)
+    {
+		var picker = sender as Picker;
+		var selectedItem = picker.SelectedItem.ToString();
+
+		addAreaButton.IsVisible = false;
+		addCabinButton.IsVisible = false;
+		addServiceButton.IsVisible = false;
+
+		switch (selectedItem)
+		{
+            case "Cabins":
+                addCabinButton.IsVisible = true;
+                break;
+            case "Services":
+                addServiceButton.IsVisible = true;
+                break;
+            case "Areas":
+                addAreaButton.IsVisible = true;
+                break;
+        }
+    }
+
+    private void addCabinButton_Clicked(object sender, EventArgs e)
     {
 
     }
 
-    private void RemoveButtonClicked(object sender, EventArgs e)
+    private void addServiceButton_Clicked(object sender, EventArgs e)
     {
 
     }
 
-    private void AreaListViewItemTapped(object sender, SelectedItemChangedEventArgs e)
+    private void addAreaButton_Clicked(object sender, EventArgs e)
     {
 
     }
