ab2c2dfc28480c6b0fe139d8c254bd60dc2cf4ee
pietikainen <pietikainen@gmail.com>
2024-04-03 14:02:36 +0300

    Force window to open on 1280x960

--- varausjarjestelma/App.xaml.cs
+++ varausjarjestelma/App.xaml.cs
@@ -2,6 +2,21 @@
 {
     public partial class App : Application
     {
+
+        protected override Window CreateWindow(IActivationState activationState)
+        {
+            var window = base.CreateWindow(activationState);
+
+            // Change the window Size
+            window.Width = 1280; window.Height = 960;
+
+            // BONUS -> Center the window
+            var displayInfo = DeviceDisplay.Current.MainDisplayInfo;
+            window.X = (displayInfo.Width / displayInfo.Density - window.Width) / 2;
+            window.Y = (displayInfo.Height / displayInfo.Density - window.Height) / 2;
+            return window;
+        }
+
         public App()
         {
             InitializeComponent();
