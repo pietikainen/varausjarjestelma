67464e575d9343ee8c80404931444abddb5b276b
Kniippala <niko.jarvela@hotmail.fi>
2024-04-03 16:59:28 +0300

    Added new modal page

---
+++ varausjarjestelma/AddCabinModal.xaml.cs
@@ -0,0 +1,19 @@
+namespace varausjarjestelma;
+
+public partial class AddCabinModal : ContentPage
+{
+	public AddCabinModal()
+	{
+        InitializeComponent();
+    }
+
+    private void cancelCabinButton_Clicked(object sender, EventArgs e)
+    {
+
+    }
+
+    private void addCabinButton_Clicked(object sender, EventArgs e)
+    {
+
+    }
+}
