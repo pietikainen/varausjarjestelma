7eb7499be762df7cd4135e26beebad043dfcc188
Kniippala <niko.jarvela@hotmail.fi>
2024-04-03 16:59:42 +0300

    added new modal page

---
+++ varausjarjestelma/AddServiceModal.xaml.cs
@@ -0,0 +1,19 @@
+namespace varausjarjestelma;
+
+public partial class AddServiceModal : ContentPage
+{
+	public AddServiceModal()
+	{
+		InitializeComponent();
+	}
+
+    private void SaveServiceButton_Clicked(object sender, EventArgs e)
+    {
+
+    }
+
+    private void CancelServiceButton_Clicked(object sender, EventArgs e)
+    {
+
+    }
+}
